4fc361041b5ec5cd3bcee8f1ed6a5cfcdb5196c7
Tanmay Kumar <itstanmaykumar@gmail.com>
2021-10-18 02:10:03 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBxIrdVkQvwSQTzvBeIlktyTRPNw4Wbjiw",
+    authDomain: "mindcare-e6f9d.firebaseapp.com",
+    projectId: "mindcare-e6f9d",
+    storageBucket: "mindcare-e6f9d.appspot.com",
+    messagingSenderId: "695549268990",
+    appId: "1:695549268990:web:a42a43b780dc671812a6af"
+  };
+
+  export default firebaseConfig;
