bfc0559ecdd5a458a4e62dceb02e6a92367ed0e7
Tanmay Kumar <itstanmaykumar@gmail.com>
2021-10-18 15:17:54 +0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './Components/Home/Home';
 import Services from './Components/Services/Services';
 import Blogs from './Components/Blogs/Blogs';
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../logo.png'
+import logo from '../../logo.png';
 
 const Header = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
-                <div class="container">
-                    <Link class="navbar-brand" to="/home">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
+                <div className="container">
+                    <Link className="navbar-brand" to="/home">
                         <img className="me-2 pb-1" width="20" src={logo} alt="mindcare logo" />
-                        <span className="fw-bolder">mindcare</span>
+                        <span className="fw-bolder text-main">mindcare</span>
                     </Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
-                        <li class="nav-item me-2">
-                            <Link class="nav-link" to="/services">Find therapy</Link>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
+                        <li className="nav-item me-2">
+                            <Link className="nav-link" to="/services">Find therapy</Link>
                         </li>
-                        <li class="nav-item me-2">
-                            <Link class="nav-link" to="/blogs">Bolgs</Link>
+                        <li className="nav-item me-2">
+                            <Link className="nav-link" to="/blogs">Bolgs</Link>
                         </li>
-                        <li class="nav-item me-2">
-                            <Link class="nav-link" to="/info">Info</Link>
+                        <li className="nav-item me-2">
+                            <Link className="nav-link" to="/info">Info</Link>
                         </li>
                         <li className="me-5"></li>
-                        <li class="nav-item">
-                            <Link class="btn btn-outline-main fw-bolder" to="/join">Join Us</Link>
+                        <li className="nav-item">
+                            <Link className="btn btn-outline-main fw-bolder" to="/join">Join Us</Link>
                         </li>
                     </ul>
                     </div>
